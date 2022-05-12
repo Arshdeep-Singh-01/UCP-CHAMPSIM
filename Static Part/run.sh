2a2e3c7ce394b4e9d62c93b8ce6ca8005c775250
Ankit Sharma <75074904+CrypticMessenger@users.noreply.github.com>
2022-05-12 17:47:49 +0530

    Delete Static Part directory

--- Static Part/run.sh
+++
@@ -1,5 +0,0 @@
-chmod +x build_champsim.sh
-./build_champsim.sh bimodal no no no no lru 2
-chmod +x run_2core.sh
-sed -i 's/\r//' run_2core.sh
-./run_2core.sh bimodal-no-no-no-no-lru-2core 1 100 0 bzip2_183B.trace.xz bzip2_183B.trace.xz
